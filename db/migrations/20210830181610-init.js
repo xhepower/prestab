064e9e1f08586c1809159f819c12f93e1af5dcc7
xhepower <hbuezoss@gmail.com>
2022-11-28 21:23:38 -0600

    hello

---
+++ db/migrations/20210830181610-init.js
@@ -0,0 +1,288 @@
+'use strict';
+
+const { USER_TABLE } = require('./../models/user.model');
+const { INGRESO_TABLE } = require('./../models/ingreso.model');
+const { EGRESO_TABLE } = require('./../models/egreso.model');
+const { CUENTA_TABLE } = require('./../models/cuenta.model');
+const { CONCEPTO_TABLE } = require('./../models/concepto.model');
+const { PDF_TABLE } = require('./../models/pdf.model');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      recoveryToken: {
+        allowNull: true,
+        type: Sequelize.DataTypes.STRING,
+        field: 'recovery_token',
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+        defaultValue: 'usuario',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
+    //# de aqui////////////////
+
+    await queryInterface.createTable(CONCEPTO_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
+    await queryInterface.createTable(CUENTA_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
+    await queryInterface.createTable(PDF_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      idNumber: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+        field: 'id_number',
+      },
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      city: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      address: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      phone: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      passport: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      purpose: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      issued: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      refused: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      file: {
+        type: Sequelize.DataTypes.STRING,
+        unique: true,
+        allowNull: true,
+      },
+      date: {
+        type: Sequelize.DataTypes.DATE,
+        unique: false,
+        allowNull: true,
+      },
+      photo: {
+        type: Sequelize.DataTypes.STRING.BINARY,
+        unique: false,
+        allowNull: true,
+      },
+      barcode: {
+        type: Sequelize.DataTypes.STRING.BINARY,
+        unique: false,
+        allowNull: true,
+      },
+      estado: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        allowNull: true,
+      },
+      fileID: {
+        type: Sequelize.DataTypes.STRING,
+        unique: false,
+        field: 'file_id',
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
+
+    await queryInterface.createTable(INGRESO_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      descripcion: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true,
+      },
+      monto: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+      },
+      tipo: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      cuentaId: {
+        field: 'cuenta_id',
+        allowNull: true,
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: CUENTA_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      conceptoId: {
+        field: 'concepto_id',
+        allowNull: true,
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: CONCEPTO_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+    });
+    await queryInterface.createTable(EGRESO_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      descripcion: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true,
+      },
+      monto: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+      },
+      tipo: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      cuentaId: {
+        field: 'cuenta_id',
+        allowNull: true,
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: CUENTA_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      conceptoId: {
+        field: 'concepto_id',
+        allowNull: true,
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: CONCEPTO_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+    });
+    //hasta aqui
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable(USER_TABLE);
+    await queryInterface.dropTable(INGRESO_TABLE);
+    await queryInterface.dropTable(EGRESO_TABLE);
+    await queryInterface.dropTable(CUENTA_TABLE);
+    await queryInterface.dropTable(CONCEPTO_TABLE);
+    await queryInterface.dropTable(PDF_TABLE);
+  },
+};
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const usersRouter = require('./users.router');
+
+const conceptosRouter = require('./conceptos.router');
+const cuentasRouter = require('./cuentas.router');
+const ingresosRouter = require('./ingresos.router');
+const egresosRouter = require('./egresos.router');
+const authRouter = require('./auth.router');
+const pdfsRouter = require('./pdfs.router');
+
+function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/users', usersRouter);
+  router.use('/ingresos', ingresosRouter);
+  router.use('/egresos', egresosRouter);
+  router.use('/cuentas', cuentasRouter);
+  router.use('/conceptos', conceptosRouter);
+  router.use('/auth', authRouter);
+  router.use('/pdfs', pdfsRouter);
+}
+
+module.exports = routerApi;
